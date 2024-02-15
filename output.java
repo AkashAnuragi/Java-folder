c932da93dd70678a248acd95b342da1ccb7a23d0
akdeveloper01 <akashanuragi543@gmail.com>
2024-02-15 10:48:36 +0530

    output

---
+++ output.java
@@ -0,0 +1,6 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("this is the way to print string under the double quote .");
+        System.out.println(300); // this is the way print a numbers you can print numbers without double quote.
+    }
+}
