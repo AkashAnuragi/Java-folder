678b733bd75f393ec05e89c536400c91a3970b7d
akdeveloper01 <akashanuragi543@gmail.com>
2024-02-16 10:55:02 +0530

    datatype

---
+++ datatype.java
@@ -0,0 +1,29 @@
+public class datatype {
+    public static void main(String[] args) {
+
+         // premitive datatype
+        byte b = 1;
+        System.out.println(b);
+
+        short s = 23 ;
+        System.out.println(s);
+
+        int i = 435435;
+        System.out.println(i);
+
+        long l = 546646446;
+        System.out.println(l);
+
+        float  f = 3.5657f;
+        System.out.println(f);
+
+        double d = 454.454d;
+        System.out.println(d);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        char c ='a';
+        System.out.println(c);
+    }
+}
