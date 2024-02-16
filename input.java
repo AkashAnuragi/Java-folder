3a0b7fcb2b47c585dbec0641a3a482f7abeabaac
akdeveloper01 <akashanuragi543@gmail.com>
2024-02-16 12:12:48 +0530

    input

---
+++ input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int i = sc.nextInt();
+        // System.out.println(i);
+
+        // float f = sc.nextFloat();
+        // System.out.println(f);
+
+        boolean bool = sc.nextBoolean();
+        System.out.println(bool);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+   int a,b,add ;
+   a = 10;
+   b = 12; 
+   add = a + b;
+    System.out.println(add); 
+    }
+}
