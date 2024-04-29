9ca01bc27216ea88493e603d960391ae39c110fc
akdeveloper01 <akashanuragi543@gmail.com>
2024-04-29 23:59:01 +0530

    63

---
+++ string1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class string1 {
+    public static void main(String[] args) {
+        char ch[] = {'a', 'b', 'c', 'd'};
+        String str ="Akash Anuragi";
+        String str2 =new String("Rajendra Anuragi");
+        System.err.println(ch );
+        System.err.println(str2);
+        System.err.println(str);
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
