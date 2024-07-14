5233abd03dcd9d28236037ae2140e2304a8d8fb8
akdeveloper01 <akashanuragi543@gmail.com>
2024-07-14 19:24:52 +0530

    75

---
+++ bit_manipulation.java
@@ -0,0 +1,23 @@
+public class bit_manipulation{
+    public static void main(String[] args){
+    // Bit Manipulation.
+     
+    //  1. Bitwise AND
+    System.out.println( "Bitswise AND Operator : " + (5&6));
+
+    // 2. Bitwise OR
+    System.out.println( "Bitwise OR Operator : " +(5|6) );
+
+    // 3. Bitwise XOR
+    System.out.println( "Bitwise XOR Operator : " +(5^6));
+
+    // 4. Binary One's Complement
+    System.out.println( "Binary One's Complement : " +(~5));
+
+    // 5. Binary Right Shift
+    System.out.println(" Binary Right Shift : " +(5>>2));
+
+    //6. Binary Left Shift 
+    System.out.println("Binary Left Shift : " + (5<<2));
+    }
+}
