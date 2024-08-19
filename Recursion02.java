591a92c84ac21fc20c5ec1fbed7252a8f0537b22
akdeveloper01 <akashanuragi543@gmail.com>
2024-08-19 09:04:54 +0530

    108

---
+++ Recursion02.java
@@ -0,0 +1,27 @@
+public class Recursion02 {
+    public static void printInc( int n){
+        if (n == 10) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printInc(n + 1);
+    }
+// Another method.
+    public static void Inc( int n){
+        if (n == 1) {
+            System.out.print( "\n" + n + " ");
+            return;
+        }
+        Inc(n-1);
+        System.out.print(n + " ");
+    }
+    
+
+    public static void main(String[] args) {
+        int n = 1;
+        int m = 10;
+        printInc(n);
+        Inc(m);
+    }
+}
