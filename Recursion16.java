01571d0872a21dac3fe737c8798ea3240d6a2546
AkashAnuragi <akashanuragi543@gmail.com>
2024-08-26 17:40:32 +0530

    122

--- Recursion16.java
+++ Recursion16.java
@@ -1,3 +1,7 @@
+/*You are given a number (eg 2019), convert it into a String of english like "two zero one nine". Use a recursive function to solve this problem. 
+ Input: 1947
+Sample Output: "one nine four seven" */
+
 public class Recursion16 {
         static String digits[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
         public static void PrintDigits(int number){
