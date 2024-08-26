2ddc105ee0bdc55541a5048e9912a42f101a3b93
AkashAnuragi <akashanuragi543@gmail.com>
2024-08-26 17:44:34 +0530

    121

--- Recursion15.java
+++ Recursion15.java
@@ -1,3 +1,9 @@
+/* For a given integer array of size N. You have to find all the occurrences (indices) of a given element (Key) and print them. Use a recursive function to solve this problem.
+
+Sample Input: arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2), key = 2
+Sample Output: 1578 
+*/
+
 public class Recursion15 {
     public static void FindAllOccurance(int arr[], int key, int i){
         // Base Case
