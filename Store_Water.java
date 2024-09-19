410134a75d328040a725b93576cbec480fb8a06b
AkashAnuragi <akashanuragi543@gmail.com>
2024-09-19 11:27:24 +0530

    142. Store Water

---
+++ Store_Water.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Store_Water {
+    public static int storeWater(ArrayList<Integer> height){
+    int maxWater = 0;
+
+    // Brute Force
+    for(int i= 0; i<height.size(); i++){
+        for(int j = i+1; j<height.size(); j++){
+            int ht = Math.min(height.get(i), height.get(j));
+            int width = j-i;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+        }
+    }
+    return maxWater;
+}
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(9);
+        System.out.println(" The Maximum Stored Water is : " + storeWater(height ));
+    }
+}
+
+// Time Comlexity - O(N^2)
