cf96dc3e7f03a3706fc10f0ae0ecb21ea88781fc
AkashAnuragi <akashanuragi543@gmail.com>
2024-09-19 11:57:40 +0530

    144. Pain Sum -1

---
+++ Pair_sum.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Pair_sum {
+    public static boolean pairSum(ArrayList<Integer> list, int target){
+        for(int i = 0;i<list.size(); i++){
+            for(int j = i+1;j<list.size(); j++){
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        int target = 9;
+        // int target = 23;
+        System.out.println(pairSum(list, target));
+    }
+}
